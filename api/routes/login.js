96e96de6cc5f6e56d08d7a6b48ffdfd6e8f2b580
Gerhard Klemola <gerhardcarlklemola@protonmail.com>
2021-11-21 21:27:34 +0200

    login alkunaytto toimii

--- api/routes/login.js
+++ api/routes/login.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const login = require('../models/login_model');
+const login = require('../models/loginID_model');
 
 router.get('/:id',
   function(request, response) {
