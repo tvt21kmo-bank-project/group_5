b26b632d729fdab6f55e5353a92e96c8fe8db15c
Gerhard Klemola <gerhardcarlklemola@protonmail.com>
2021-11-23 16:45:36 +0200

    lisatty try-catch toiminto

--- api/routes/creditdebit.js
+++ api/routes/creditdebit.js
@@ -5,17 +5,22 @@ const creditDebit = require('../models/creditdebit_model');
 router.get('/:idkortti',
   function(req, res) {
     creditDebit.checkAccType(req.params.idkortti, function(dbError, dbResult) {
+      try {
       if(dbError) {
         res.json(dbError);
         console.log("Tapahtui virhe!");
       } else {
-        var tyyppi = dbResult[0].tilityyppi
+        let tyyppi = dbResult[0].tilityyppi
         if(tyyppi === 'credit,debit') {
           res.send(true);
         } else {
           res.send(false);
         }
       }
+    } catch(e) {
+      console.log("Does not exist");
+      res.send("Does not exist");
+    }
 
 });
 });
