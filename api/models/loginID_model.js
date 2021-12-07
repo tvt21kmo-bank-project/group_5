92c850fd2149882adc924b190f1ada121dc628fb
Gerhard Klemola <gerhardcarlklemola@protonmail.com>
2021-12-07 20:50:53 +0200

    tekstien lähetys päivitetty

--- api/models/loginID_model.js
+++
@@ -1,9 +0,0 @@
-const db = require('../database.js');
-
-const login = {
-  checkID: function(idkortti, callback) {
-      return db.query('select idkortti from kortti where idkortti = ?', [idkortti], callback);
-    }
-};
-
-module.exports = login;
